6c42b865b6b12867d356a0003389ed984d0a7d12
Luis Eduardo Brito <luis@devnup.com>
2016-04-01 00:55:32 -0300

    Feat: UI integrated with electron

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # mc714-2s2016-lista3
-Socket.io based chat server and clients, implemented in NodeJS
+Socket.io based chat server and clients, implemented in NodeJS for the Distributed Systems course (MC714 1s2016) with Prof. Leandro Villas.
+
+Inspired in the [Example Chat Application](http://chat.socket.io) from the [Socket.io Official Documentation](http://socket.io/get-started/chat/).
 
 
-### Authors
-- Luis Eduardo Brito <luiseduardo14@gmail.com>
+Checkout the [live preview (coming soon, hosted in Heroku)](https://radiant-river-70847.herokuapp.com/ )
+
+### Open Source Dependencies
+
+Desktop app client by David (@retyui)
+
+- Openly available at: http://codepen.io/retyui/pen/zxGqPJ
+
+
+### Author
+- Luis Eduardo Brito (RA138760) <luis.brito@students.ic.unicamp.br>
---
+++ client/js/style.js
@@ -0,0 +1,56 @@
+(function () {
+  var NYLM, claerResizeScroll, conf, getRandomInt, insertI, lol;
+
+  conf = {
+    cursorcolor: "#696c75",
+    cursorwidth: "4px",
+    cursorborder: "none"
+  };
+
+  lol = {
+    cursorcolor: "#cdd2d6",
+    cursorwidth: "4px",
+    cursorborder: "none"
+  };
+
+  NYLM = ["Уходи дверь закрой", "У меня теперь другой", "Все для тебя", "Мне не нужен больше твой номер в книжке записной", "Владимирский централ, ветер сука", "Ты ушол, а я текла", "Ты пришол в красный день календаря", "бла бла", ")", "умри", "ой все.", "ой все.", "ой все.", "Ты говоришь ТОЧНЕЕ пишешьСя сам с собой"];
+
+  getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  claerResizeScroll = function () {
+    $("#texxt").val("");
+    $(".messages").getNiceScroll(0).resize();
+    return $(".messages").getNiceScroll(0).doScrollTop(999999, 999);
+  };
+
+  insertI = function () {
+    var innerText, otvet;
+    innerText = $.trim($("#texxt").val());
+    if (innerText !== "") {
+      $(".messages").append("<li class=\"i\"><div class=\"head\"><span class=\"time\">" + (new Date().getHours()) + ":" + (new Date().getMinutes()) + " AM, Today</span><span class=\"name\"> Буль</span></div><div class=\"message\">" + innerText + "</div></li>");
+      claerResizeScroll();
+      return otvet = setInterval(function () {
+        $(".messages").append("<li class=\"friend-with-a-SVAGina\"><div class=\"head\"><span class=\"name\">Юния  </span><span class=\"time\">" + (new Date().getHours()) + ":" + (new Date().getMinutes()) + " AM, Today</span></div><div class=\"message\">" + NYLM[getRandomInt(0, NYLM.length - 1)] + "</div></li>");
+        claerResizeScroll();
+        return clearInterval(otvet);
+      }, getRandomInt(2500, 500));
+    }
+  };
+
+  jQuery(document).ready(function () {
+    jQuery(".list-friends").niceScroll(conf);
+    jQuery(".messages").niceScroll(lol);
+    jQuery("#texxt").keypress(function (e) {
+      if (e.keyCode === 13) {
+        insertI();
+        return false;
+      }
+    });
+    return $(".send").click(function () {
+      return insertI();
+    });
+  });
+
+}).call(this);
